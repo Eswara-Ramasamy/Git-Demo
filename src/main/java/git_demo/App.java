7e9c73faf3a91818014e533ad35d0cabf8b8f9a1
Eswara Ramasamy <eswar.sankar@gmail.com>
2023-03-05 16:57:19 +0530

    Git commit to the local repository

---
+++ src/main/java/git_demo/App.java
@@ -0,0 +1,15 @@
+package git_demo;
+
+import java.sql.SQLOutput;
+
+public class App {
+
+    public void addData(String name) {
+        System.out.println("Data = " +name);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
